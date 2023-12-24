9e6a341b9820fd2ec3ae16e39a02cc6d2aca5b85
Temirov Ruslan <ruslan.temirov1995@gmail.com>
2023-12-24 13:06:36 +0300

    Added cards

--- app/[lang]/components/BeefCards.tsx
+++ app/[lang]/components/BeefCards.tsx
@@ -1,14 +1,31 @@
 import { beef } from '@/constant'
+import Image from 'next/image'
 
 const BeefCards = () => {
   return (
-    <ul className='grid grid-cols-1 gap-8 sm:grid-cols-2 md:grid-cols-3'>
+    <ul className='grid grid-cols-1 gap-10 sm:grid-cols-2 md:grid-cols-3'>
       {beef &&
         beef.map((item, i) => {
           return (
-            <li className='h-[300px] rounded-lg bg-[#1a5730] p-2' key={i}>
-              <p>{item.name}</p>
-              <p>€{item.price} per/kg</p>
+            <li
+              className='h-[300px] rounded-lg bg-[#1a5730] py-5 text-white'
+              key={i}
+            >
+              <div className='flex h-[80%] w-full items-center justify-center'>
+                <div className='relative'>
+                  <Image
+                    src={item.image}
+                    height={170}
+                    width={200}
+                    // objectFit='cover'
+                    alt='Picture of the author'
+                  />
+                </div>
+              </div>
+              <div className='ml-14 h-[20%]'>
+                <p>{item.name}</p>
+                <p>€{item.price} per/kg</p>
+              </div>
             </li>
           )
         })}
--- constant/index.ts
+++ constant/index.ts
@@ -1,72 +1,60 @@
-import { lambType } from "@/types";
+import { lambType } from '@/types'
+import img_1 from '/public/beef_meat1.webp'
+import img_2 from '/public/beef_meat2.webp'
+import img_3 from '/public/beef_meat3.webp'
+import img_4 from '/public/beef_meat4.webp'
+import img_5 from '/public/beef_meat5.webp'
+import img_6 from '/public/beef_meat6.webp'
+import img_7 from '/public/beef_meat7.webp'
+import img_8 from '/public/beef_meat8.webp'
 
 export const lamb: lambType[] = [
-	{
-		price: 15,
-		name: "Beef Chunk",
-		image: "",
-	},
-];
+  {
+    price: 15,
+    name: 'Beef Chunk',
+    image: img_1.src
+  }
+]
 
 export const beef: lambType[] = [
-	{
-		price: 15,
-		name: "Beef Chunk",
-		image: "",
-	},
-	{
-		price: 15,
-		name: "Beef Chunk",
-		image: "",
-	},
-	{
-		price: 15,
-		name: "Beef Chunk",
-		image: "",
-	},
-	{
-		price: 15,
-		name: "Beef Chunk",
-		image: "",
-	},
-	{
-		price: 15,
-		name: "Beef Chunk",
-		image: "",
-	},
-	{
-		price: 15,
-		name: "Beef Chunk",
-		image: "",
-	},
-	{
-		price: 15,
-		name: "Beef Chunk",
-		image: "",
-	},
-	{
-		price: 15,
-		name: "Beef Chunk",
-		image: "",
-	},
-	{
-		price: 15,
-		name: "Beef Chunk",
-		image: "",
-	},
-	{
-		price: 15,
-		name: "Beef Chunk",
-		image: "",
-	},
-	{
-		price: 15,
-		name: "Beef Chunk",
-		image: "",
-	},
-	{
-		price: 15,
-		name: "Beef Chunk",
-		image: "",
-	},
-];
+  {
+    price: 15,
+    name: 'Beef Chunk',
+    image: img_1.src
+  },
+  {
+    price: 15,
+    name: 'Beef Chunk',
+    image: img_2.src
+  },
+  {
+    price: 15,
+    name: 'Beef Chunk',
+    image: img_3.src
+  },
+  {
+    price: 15,
+    name: 'Beef Chunk',
+    image: img_4.src
+  },
+  {
+    price: 15,
+    name: 'Beef Chunk',
+    image: img_5.src
+  },
+  {
+    price: 15,
+    name: 'Beef Chunk',
+    image: img_6.src
+  },
+  {
+    price: 15,
+    name: 'Beef Chunk',
+    image: img_7.src
+  },
+  {
+    price: 15,
+    name: 'Beef Chunk',
+    image: img_8.src
+  }
+]
